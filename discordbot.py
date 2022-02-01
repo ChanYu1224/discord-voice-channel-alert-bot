87f94af4085bc11d31946e836ba22fac0483c6ef
ChanYu1224 <kinkitunekin@gmail.com>
2022-02-01 14:48:03 +0900

    fix datetime

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,6 @@
 import discord
 import datetime
+import pytz
 from os import getenv
 
 TOKEN = getenv('DISCORD_BOT_TOKEN')
@@ -7,14 +8,15 @@ MESSAGE_ROOM = 937677135349440513
 
 client = discord.Client()
 
-def datetime_message(datetime_now:datetime.datetime):
+def datetime_message():
+    datetime_now = datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
     return str(datetime_now.year)+'/'+str(datetime_now.month)+'/'+str(datetime_now.day)+' '+str(datetime_now.hour)+':'+str(datetime_now.minute)+':'+str(datetime_now.second)
 
 @client.event
 async def on_voice_state_update(member:discord.Member, before, after):
     if before.channel != after.channel:
         message_room = client.get_channel(MESSAGE_ROOM)
-        now_time = datetime_message(datetime.datetime.now())
+        now_time = datetime_message()
 
         #enter alert
         if before.channel is None:
