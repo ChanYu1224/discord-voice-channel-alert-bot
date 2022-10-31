13914e341d94fd2801fa1359b13ab0a110e7a6bc
Yuta Nishikawa <yuta@yuta-macbook-pro.local>
2022-11-01 08:37:58 +0900

    [ADD] script to getting chennel IDs and names

---
+++ get_room_id.py
@@ -0,0 +1,22 @@
+import discord
+import json
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+with open("./dev_settings.json", mode="r") as setting_file:
+    setting_dict = json.load(setting_file)
+
+TOKEN = setting_dict["TOKEN"]
+
+@client.event
+async def on_ready():
+    for channel in client.get_all_channels():
+        print("---------------")
+        print("channel name", str(channel.name))
+        print("channel ID", str(channel.id))
+    print("---------------")
+    
+    exit()
+
+client.run(TOKEN)
