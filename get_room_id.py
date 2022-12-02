45077cd6a1df7eb566452bb3a0ebb97cc806ced3
Yuta Nishikawa <kinkitunekin@gmail.com>
2022-12-02 14:26:57 +0900

    [FIX] adapt for new setting format

--- get_room_id.py
+++ get_room_id.py
@@ -4,10 +4,10 @@ import json
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
-with open("./dev_settings.json", mode="r") as setting_file:
+with open("./settings.json", mode="r") as setting_file:
     setting_dict = json.load(setting_file)
 
-TOKEN = setting_dict["DISCORD_BOT_TOKEN"]
+TOKEN = setting_dict["token"]
 
 @client.event
 async def on_ready():
